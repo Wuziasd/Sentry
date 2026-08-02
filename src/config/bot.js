6d9230b0a2a9a0fc6f433b53aab0aa47fecd0eac
Wuziasd <WuziAztech@outlook.com>
2026-08-02 22:23:55 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Kaya Benim Köpeğim", // required by Discord API, not shown in the client
         state: "discord.gg/rtat",     // this is what people actually see
         type: 0,               // Custom
       },
